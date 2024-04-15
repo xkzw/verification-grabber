152befd70bd0c141961c4a1993a49e82612b9068
nosztalgia <166217460+nosztalgia@users.noreply.github.com>
2024-04-15 22:55:13 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,21 +1,21 @@
 <?php
 
-// Created by nosztalgia (lv8)
-// My discord server: https://discord.gg/yG4my2nqTa
-// Credits to https://github.com/MarkisDev
+# Created by nosztalgia (lv8)
+# My discord server: https://discord.gg/yG4my2nqTa
+# Credits to https://github.com/MarkisDev
 
-// BOT INFORMATION
+# BOT INFORMATION
 
 $client_id = "CLIENT_ID";
 $secret_id = "SECRET_ID";
 $bot_token = "BOT_TOKEN";
 
-// DEV APPLICATION
+# DEV APPLICATION
 
-$scopes = "identify+email"; // NE PAS CHANGER !
+$scopes = "identify+email"; # NE PAS CHANGER !
 $redirect_url = "https://VOTRESITE.COM/includes/login.php";
 
-// SERVER INFORMATION
+# SERVER INFORMATION
 
 $guild_id = "SERVER_ID";
 $role_id = "ROLE_VERIFIE_ID";
